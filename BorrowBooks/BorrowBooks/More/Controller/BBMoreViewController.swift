9b816f0385ccfe65a4f7defa6d6112667d7c57c1
Haibin Wang <hbwang85@gmail.com>
2015-03-25 17:56:28 +0800

    #5 UI for login and register

--- BorrowBooks/BorrowBooks/More/Controller/BBMoreViewController.swift
+++ BorrowBooks/BorrowBooks/More/Controller/BBMoreViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class BBMoreViewController: UITableViewController {
-    
-//    var sectionArray
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,48 +16,13 @@ class BBMoreViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        var rows: Int
-        
-        switch section {
-        case 0:
-            rows = 2
-        case 1:
-            rows = 3
-        case 2:
-            rows = 1
-        default:
-            rows = 0
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated:false)
+        if (indexPath.row==0 && indexPath.section==0){
+            let loginSB = UIStoryboard(name: "Login", bundle: nil)
+            let loginVC = loginSB.instantiateInitialViewController() as UIViewController
+            loginVC.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(loginVC, animated: true)
         }
-        
-        return rows
     }
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int{
-        return 3
-    }
-    
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        return 1
-//    }
-//    
-//    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        return 1
-//    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
